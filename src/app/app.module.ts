586816b1198ad00c071b18069811fedd09257094
mariamiecho <maria.miechowiecka@gmail.com>
2023-11-02 22:54:01 +0300

    Upgrading things added durning classes
    Giving functions more pleasing to the eye looks. Adding descriptions, comments, updating functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersComponent } from './customers/customers.component';
@@ -22,7 +23,8 @@ import { SummaryPipe } from './summary.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule
   ],
   providers: [
     CustomersService],
--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -21,20 +21,21 @@ export class CustomersComponent {
   onKeyUp() {
     console.log("Enter was pressed")
   }
+  email = "email@example.com";
+  onKeyUpEmail() {
+    console.log(this.email);
+  }
 
-  // onKeyUpEmail(email: any) {
-  //   console.log("enter")
 
   customer = {
-    title: "Customers details:",
+    title: "Hotel details:",
     rating: 3.45673,
-    amount: 3294,
+    rooms: 13,
     price: 2137,
-    freeDate: new Date(2054, 11, 11)
-
-
-
+    freeDate: new Date(2054, 10, 11)
   }
+
+  text = 'Welcome to My-Hotel, a place where warm hospitality meets contemporary charm. Our hotel, nestled in the vibrant heart of the city, is your ideal choice for a comfortable and friendly stay, whether you are traveling for business or leisure.'
 
 }
 
