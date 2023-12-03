a6d19647f81c18f839d63055d8e132258183e382
mariamiecho <maria.miechowiecka@gmail.com>
2023-12-03 19:54:37 +0300

    Make my form even better

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReservationValidators } from './reservation.validators';
 // import { ReservationValidators } from './reservation.validators';
 
 
@@ -10,12 +11,16 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class ReservationFormComponent {
   form = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    name: new FormControl('', [Validators.required, Validators.minLength(3), ReservationValidators.cannotContainSpace]),
     // , ReservationValidators.cannotContainSpace]),
-    price: new FormControl('', Validators.required)
+    price: new FormControl('', [Validators.required, ReservationValidators.minPrice]),
     // he wants customerName i price
   });
   get name() {
     return this.form.get('name');
+
+  }
+  get price() {
+    return this.form.get('price');
   }
 }
--- src/app/reservation-form/reservation.validators.ts
+++ src/app/reservation-form/reservation.validators.ts
@@ -1,12 +1,21 @@
-// import { AbstractControl, Validator } from "@angular/forms";
+import { AbstractControl, Validators } from "@angular/forms";
 
-// ReservationValidators.cannotContainSpace
-// export class ReservationValidators {
-//     static cannotContainSpace(control: AbstractControl) {
-//         if ((control.value as string).indexOf(' ') >= 0)
-//             return { cannotContainSpace: true };
-//         return null
-//     }
-// }
 
+
+export class ReservationValidators {
+    static cannotContainSpace(control: AbstractControl) {
+        if ((control.value as string).indexOf(' ') >= 0)
+            return { cannotContainSpace: true };
+        return null
+    }
+
+    static minPrice(control: AbstractControl) {
+        const value = control.value as number;
+        return Validators.min(9)(control);
+    }
+}
 // minmum price is 10, actual price = control.that?? value
+//return {minlength: {
+//requiredLength: 10,
+//actualLength: control.value.length
+// }}
